e0e5e74e23ac2e15719867c82f24a003d9536c38
Danila Vaganov <danwhale@MacBook-Pro-Danila.local>
2022-12-19 12:01:58 +0300

    fix path

--- tests/utils.py
+++ tests/utils.py
@@ -3,10 +3,10 @@ import pathlib
 
 
 def read_test_data():
-    path = pathlib.Path(__file__).parent.resolve().joinpath("/data/")
-    train = pd.read_csv(path.joinpath("/train.csv"))
-    test = pd.read_csv(path.joinpath("data/test.csv"))
-    valid = pd.read_csv(path.joinpath("data/valid.csv"))
+    path = pathlib.Path(__file__).parent.resolve().joinpath("data")
+    train = pd.read_csv(path.joinpath("train.csv"))
+    test = pd.read_csv(path.joinpath("test.csv"))
+    valid = pd.read_csv(path.joinpath("valid.csv"))
 
     for i in [train, test, valid]:
         i["neg_head"] = [eval(l) for l in i["neg_head"]]
