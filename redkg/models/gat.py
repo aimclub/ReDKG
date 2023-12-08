e3a79bb89e9723c68c432c4d1549b983aff74476
Paul Rock <paul@drteam.rocks>
2023-12-08 21:55:35 +0300

    isort fix

--- redkg/models/gat.py
+++ redkg/models/gat.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
-from torch_geometric.nn import GATConv
 import torch.nn.functional as F
+from torch_geometric.nn import GATConv
 
 
 class GAT(torch.nn.Module):
--- redkg/models/gcn.py
+++ redkg/models/gcn.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
-from torch_geometric.nn import GCNConv
 import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
 
 
 class GCN(torch.nn.Module):
